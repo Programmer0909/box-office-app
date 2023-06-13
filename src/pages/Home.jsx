a3c27986ab54f21a18f8129c795cc56e7a7ad17f
Pratham08 <prathampali0909@gmail.com>
2023-06-13 13:15:47 +0530

    added api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 const Home = () => {
+  const [searchStr, setSearchStr] = useState("hyi");
+
+  function onSearchInputChange(event) {
+    const val = event.target.value;
+    setSearchStr(val);
+  }
+
+  const onSearch = async (ev) => {
+    ev.preventDefault();
+    const url = `https://api.tvmaze.com/search/shows?q=${searchStr}`;
+    // console.log(url);
+    const response = await fetch(url);
+    const body = await response.json();
+    // console.log(body[0].show.name);
+    body.map(item => console.log(item.show.name));
+
+  };
+
   return (
     <div>
-      <h1>Home</h1>
-      {/* <h1><Link to="/starred" > Starred</Link></h1> */}
+      <form onSubmit={onSearch}>
+        <input type="text" onChange={onSearchInputChange} value={searchStr} />
+        <button type="submit"> Search </button>
+      </form>
     </div>
   );
 };
