b6f35b1ccf8c1cac8ab5da7004cde41a3bc3b9fd
Pratham08 <prathampali0909@gmail.com>
2023-06-13 11:26:18 +0530

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter ,  Routes , Route } from "react-router-dom";
+import  Home  from "./pages/Home";
+import  Starred  from "./pages/Starred";
 import React from 'react';
 function App() {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/starred" element = {<Starred />} />
+        <Route path="*" element = {<div> Not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
